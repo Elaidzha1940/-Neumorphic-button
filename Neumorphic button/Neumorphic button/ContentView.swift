1c95c2ae4bc594d081a63a6ca51c688ddaa26f49
Elaidzha1940 <eli080293@gmail.com>
2023-05-06 16:04:01 +0700

    Neumorphic button + added Button + added Colors

--- Neumorphic button/Neumorphic button/ContentView.swift
+++ Neumorphic button/Neumorphic button/ContentView.swift
@@ -17,9 +17,13 @@ struct ContentView: View {
            Text("Button")
                 .font(.system(size: 20, weight: .semibold, design: .rounded))
                 .frame(width: 200, height: 60)
-            
+                .background(Color.white)
+                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
         }
         .padding()
+        
+//        Color(#init(_colorLiteralRed: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)
     }
 }
 
