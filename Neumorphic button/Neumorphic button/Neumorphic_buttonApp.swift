3435b45c14da12f7418419d6211c23193b52feeb
Elaidzha1940 <eli080293@gmail.com>
2023-05-06 15:38:34 +0700

    Neumorphic button

---
+++ Neumorphic button/Neumorphic button/Neumorphic_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphic_buttonApp.swift
+//  Neumorphic button
+//
+//  Created by Elaidzha Shchukin on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphic_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
